096a0ce69fdf6ae61f473088ef90153b0acbccc8
Junkes Junior <jqmj.hef3@uea.edu.br>
2020-09-27 06:12:03 -0400

    classe RotaOrtodromica adicionada

--- TAP/lab2.java
+++ TAP/lab2.java
@@ -183,3 +183,21 @@ class AreaVolume {
 
     }
 }
+
+class RotaOrtodromica {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double t1 = scan.nextDouble();
+        double g1 = scan.nextDouble();
+        double t2 = scan.nextDouble();
+        double g2 = scan.nextDouble();
+        scan.close();
+        double r = 6371.0;
+        double d = r
+                * Math.acos(Math.sin(Math.toRadians(t1)) * Math.sin(Math.toRadians(t2)) + Math.cos(Math.toRadians(t1))
+                        * Math.cos(Math.toRadians(t2)) * Math.cos(Math.toRadians(g1) - Math.toRadians(g2)));
+
+        System.out.printf("A distancia entre os pontos (%.6f, %.6f) e (%.6f, %.6f) e de %.2f km", t1, g1, t2, g2, d);
+
+    }
+}
