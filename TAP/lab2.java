e3353309f8cf4c9d54440a1f0ed38d61cee4dc8a
Junkes Junior <jqmj.hef3@uea.edu.br>
2020-09-27 06:56:05 -0400

    classe AnimaisCedulas adicionada

--- TAP/lab2.java
+++ TAP/lab2.java
@@ -270,3 +270,40 @@ class TipoTriangulo {
 
     }
 }
+
+class AnimaisCedulas {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int cedula = scan.nextInt();
+        scan.close();
+
+        switch (cedula) {
+            case 2:
+                System.out.println("Tartaruga");
+                break;
+
+            case 5:
+                System.out.println("Garça");
+                break;
+
+            case 10:
+                System.out.println("Arara");
+                break;
+
+            case 20:
+                System.out.println("Mico-leão-dourado");
+                break;
+
+            case 50:
+                System.out.println("Onça-pintada");
+                break;
+
+            case 100:
+                System.out.println("Garoupa");
+                break;
+
+            default:
+                System.out.println("erro");
+        }
+    }
+}
